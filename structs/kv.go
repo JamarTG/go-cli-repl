67a1e41fbca79cbfdd548e9bd13a7d2964043b3d
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2026-03-10 04:15:42 -0500

    chore:save progress

--- structs/kv.go
+++ structs/kv.go
@@ -1,28 +1,32 @@
 package structs
-import "errors";
+
+import "errors"
 
 type kv struct {
-	transactions []map[string]string 
+	transactions []map[string]string
+	store        *store
 }
 
-func Newkv() *kv {
+func NewKv() *kv {
 	return &kv{
 		transactions: make([]map[string]string, 0),
+		store:        NewStore(),
 	}
 }
 
 func (kv *kv) Set(key string, value string) error {
-	
-	if(len(kv.transactions) == 0) {
+
+	if len(kv.transactions) == 0 {
 		err := errors.New("kv not long enough")
 		return err
 	}
-	
+
 	kv.transactions[len(kv.transactions)-1][key] = value
-	
+
 	return nil
 }
 
 func (kv *kv) Get() {
-	
+	// iterate from the end and search
+	// as a last resort consult the store
 }
